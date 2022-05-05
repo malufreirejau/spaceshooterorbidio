932352a5e98b3bb618be0df0813b00d4587aca3b
unknown <malufreirejau@yahoo.com.br>
2022-05-05 13:40:53 -0300

    creating laser

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function moveUp(event) {
     }
 }
 
-function moveDown() {
+function moveDown(event) {
     let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
     if(topPosition === "530px") {
         return
@@ -35,5 +35,24 @@ function moveDown() {
         yourShip.style.top = `${position}px`;
     }
 }
+
+function fireLaser() {
+    let laser = createLaserElement();
+    playArea.appendChild(laser);
+    moveLaser();
+}
+
+function createLaserElement() {
+    let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'));
+    let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'));
+    let newLaser = document.createElement('img');
+    newLaser.src = 'img/shoot.png';
+    newLaser.classList.add('laser');
+    newLaser.style.left = `${xPosition}px`;
+    newLaser.style.top = `${yPosition - 10}px`;
+    return newLaser;
+}
+
+
 
 window.addEventListener('keydown', flyShip)
