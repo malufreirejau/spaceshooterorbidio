a1cd0fa701a0640dbc6bf24f65b649c278a62aa6
unknown <malufreirejau@yahoo.com.br>
2022-05-05 14:38:56 -0300

    fixing bugs and adding aliens

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 const yourShip = document.querySelector('.player-shooter');
-const playArea = document.querySelectorAll('#main-play-area');
+const playArea = document.querySelector('#main-play-area');
+const aliensImg = ['img/monster-1.png', 'img/monster-2.png', 'img/monster-3.png'];
 
 function flyShip(event) {
-    if(event.key === 'ArrowUp') {
-        event.preventDefault();
-        moveUp();
-    } else if(event.key === 'ArrowDown') {
-        event.preventDefault();
-        moveDown();
-    } else if(event.key === " ") {
-        event.preventDefault();
-        fireLaser();
+if(event.key === 'ArrowUp') {
+    event.preventDefault();
+    moveUp();
+} else if(event.key === 'ArrowDown') {
+    event.preventDefault();
+    moveDown();
+} else if(event.key === " ") {
+    event.preventDefault();
+    fireLaser();
     }
 }
 
-function moveUp(event) {
+
+function moveUp() {
     let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
-    if(topPosition === "0 px") {
+    if(topPosition === "0px") {
         return
     } else {
         let position = parseInt(topPosition);
@@ -25,12 +27,13 @@ function moveUp(event) {
     }
 }
 
-function moveDown(event) {
+function moveDown() {
     let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
-    if(topPosition === "530px") {
+    let position = parseInt(topPosition);
+
+    if(position >= 500){
         return
     } else {
-        let position = parseInt(topPosition);
         position += 50;
         yourShip.style.top = `${position}px`;
     }
@@ -39,7 +42,7 @@ function moveDown(event) {
 function fireLaser() {
     let laser = createLaserElement();
     playArea.appendChild(laser);
-    moveLaser();
+    moveLaser(laser);
 }
 
 function createLaserElement() {
@@ -53,6 +56,43 @@ function createLaserElement() {
     return newLaser;
 }
 
+function moveLaser(laser) {
+    let laserInterval = setInterval(() => {
+        let xPosition = parseInt(laser.style.left);
 
+        if(xPosition === 340) {
+            laser.remove();
+        } else {
+            laser.style.left = `${xPosition + 8}px`
+        }
+    }, 10);
+}
+
+function createAliens() {
+    let newAlien = document.createElement('img');
+    let alienSprite = aliensImg[Math.floor(Math.random() * aliensImg.length)];
+    newAlien.src = alienSprite;
+    newAlien.classList.add('alien');
+    newAlien.classList.add('alien-transition');
+    newAlien.style.left = '370px';
+    newAlien.style.top = `${Math.floor(Math.random() * 330) + 30}px`;
+    playArea.appendChild(newAlien);
+    moveAlien(newAlien);
+}
+
+function moveAlien(alien) {
+    let moveAlienInterval = setInterval(() => {
+        let xPosition = parseInt(window.getComputedStyle(alien).getPropertyValue('left'));
+        if(xPosition <= 50) {
+            if(Array.from(alien.classList).includes('dead-alien')) {
+                alien.remove();
+            } else {
+                gameOver();
+            }
+        } else {
+            alien.style.left = `${xPosition - 4}px`;
+        }
+    }, 30);
+}
 
 window.addEventListener('keydown', flyShip)
